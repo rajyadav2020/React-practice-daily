6beac5ef07af948a0c916bd911e2c23d7ffa0d56
rajyadav2020 <rajyadavv176@gmail.com>
2026-01-09 10:38:26 +0530

    initail commit react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import './App.css'
+import Component from './Component'
+function App() {
+
+
+  return (
+    <>
+    <Component/>
+    <Component/>
+    <Component/>
+    <Component/>
+    <Component/>
+
+
+    </>
+  )
+}
+
+export default App
