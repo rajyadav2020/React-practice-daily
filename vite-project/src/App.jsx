04003aa1c6b2da520ff4b22d95b4cba5c85b3cf5
rajyadav2020 <rajyadavv176@gmail.com>
2026-01-16 21:18:38 +0530

    Learned props and usestate hook in react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,35 @@
 import { useState } from 'react'
-// import './App.css'
 import Component from './Component'
-function App() {
+import './App.css'
+
+
+
+// usestate hook returns the array , the react provides the more functionality in usestate hook by providing one extra fucntion like setcount and whenever changes occur trigger call setcount
+
+function App(){
+  let [count,setCount] = useState(0);
+  function incrementnum(){
+    count = count+1;
+    setCount(count)
+    // console.log(count);
+  }
+  function decrementnum(){
+    count = count-1;
+    setCount(count)
+    // console.log(count)
+  }
 
 
   return (
     <>
-    <Component/>
-    <Component/>
-    <Component/>
-    <Component/>
-    <Component/>
+    <div className='first'>
+    <h1>Count is:{count} </h1>
 
+    <button onClick={incrementnum}>Increment</button>
+    <button onClick={decrementnum}>Decrement</button>
+    </div>
 
     </>
   )
 }
-
 export default App
