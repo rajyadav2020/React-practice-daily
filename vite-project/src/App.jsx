33d0e5ef9f0872e681bd80c6dabfa8f4e61d0add
rajyadav2020 <rajyadavv176@gmail.com>
2026-01-19 22:57:16 +0530

    THE PASSWORD GENERATOR PROJECT USING USESTATE AND USEFFECT HOOK

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,51 @@
-import { useState,useEffect } from "react";
-import "./bgc.css"
+// import React from "react"
+import { useState,useEffect } from "react"
+import "./passgen.css"
 
 function App(){
 
-  //re-render ; call the function again
-  //useeffect hook
-  //this takes the call back function and a dependency
-  
+  const [Password,setPassword] = useState("");
+  const [length,setlength] = useState(10);
+  const [number,setnumber] = useState(false);
+  const [char,setchar] = useState(false)
 
-  const [color,setcolor] = useState("black");
+  function generatepassword(){
+    let str = "abcdefghijklmnopqrstuvwxABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(number)
+      str+="1234567890";
+    if(char)
+      str+="!@#$%^&*;()_-";
 
-  useEffect(() =>{
-    document.body.style.backgroundColor = color;
-  },[color])
+    let pass = ""
+    for(let i=0;i<length;i++){
+      pass+= str[Math.floor(Math.random()*str.length)];
+    }
 
-  
+    setPassword(pass);
+
+  }
+
+  useEffect( ()=>{
+    generatepassword()
+  },[length,number,char])
+
   return(
     <>
-    <h1>Background Color changer</h1>
-    <div className="button">
-      <button style={{backgroundColor:"red"}} onClick={()=>setcolor("red")}>Red</button>
-      <button  style={{backgroundColor:"blue"} } onClick={()=>setcolor("blue")} >Blue</button>
-      <button  style={{backgroundColor:"orange"}} onClick={()=>setcolor("orange")} >Orange</button>
-      <button  style={{backgroundColor:"green"}} onClick={()=>setcolor("green")} >Green</button>
-      <button  style={{backgroundColor:"pink"}} onClick={()=>setcolor("pink")}>Pink</button>
+
+    <h1>{Password}</h1>
+    <div className="second">
+      <input type="range" min={5} max={50} value={length} onChange={(e)=>setlength(e.target.value)} />
+      <label>Length:{length}</label>
+
+      <input type="checkbox" defaultChecked ={number} onChange={()=>setnumber(!number)}  />
+      <label>Number</label>
+
+      <input type="checkbox" defaultChecked={char} onChange={()=>setchar(!char)}  />
+      <label>Character</label>
     </div>
+
     </>
   )
 }
-
 
 export default App;
